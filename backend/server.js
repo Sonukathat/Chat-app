2d5f84805bb2dbd174a221816661b38067b3527b
sonukathat <skkhankathat93@gmail.com>
2025-10-21 15:00:51 +0530

    import db nd check connection in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv"
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
-
+connectDB();
 
 const PORT = process.env.PORT;
 
