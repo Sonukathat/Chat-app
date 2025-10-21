b3a3ff6738c7671588ee397a04d5a4f05b7a1f53
sonukathat <skkhankathat93@gmail.com>
2025-10-21 15:20:37 +0530

    model created

---
+++ backend/models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    gender:{type:String,required:true,enum:["male","female"]}
+},{timestamps:true});
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
