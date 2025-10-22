4d5b6e42928e795d99904b4abe822b73ee201bad
sonukathat <skkhankathat93@gmail.com>
2025-10-22 10:56:16 +0530

    add

---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+export const userRegister = async (req,res)=>{
+    try {
+        const {name,email,password,gender}= req.body;
+    } catch (error) {
+        
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
+import userRoute from './routes/userRoutes.js'
 
 dotenv.config();
 const app = express();
 
 connectDB();
 
+app.use('/api/user',userRoute)
+
 const PORT = process.env.PORT;
 
-app.get('/',(req,res)=>{
-  res.send("hello......");
-})
+// app.get('/',(req,res)=>{
+//   res.send("hello......");
+// })
 
 app.listen(PORT,()=>{
     console.log(`server runnig on ${PORT}`)
