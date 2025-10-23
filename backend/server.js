c0cf395e8d103c619022988010719d072c4fa077
sonukathat <skkhankathat93@gmail.com>
2025-10-23 11:22:58 +0530

    add vercel file for deploye

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+  res.send("hello...")
+})
+
 app.use("/api/user", userRoutes);
 app.use("/api/messages", messageRoutes);
 
